99ee70d3688b4e06a95613994308a8d2506b6afc
hspandher <hspandher@hspandher-Latitude-3330.(none)>
2014-02-11 17:48:53 +0530

    Fix csrf error issue

--- mini_shine/views.py
+++ mini_shine/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from django.views.decorators.csrf import csrf_protect
+from django.template import RequestContext
 from mini_shine.forms import RegistrationForm
 
 # Create your views here.
@@ -7,7 +9,9 @@ from mini_shine.forms import RegistrationForm
 def home(request):
     return render_to_response('home.html')
 
+@csrf_protect
 def register(request):
     form = RegistrationForm()
-    return render_to_response('register.html', { 'form': form })
+    csrfContext = RequestContext(request)
+    return render_to_response('register.html', { 'form': form }, csrfContext)
 
