8bec9417a510182d438e2c402762486ba700d0e0
hspandher <hspandher@hspandher-Latitude-3330.(none)>
2014-02-11 10:43:13 +0530

    point register now link to register view

--- mini_shine/tests.py
+++ mini_shine/tests.py
@@ -7,7 +7,7 @@ from mini_shine.views import home
 
 class HomePageTest(TestCase):
 
-    def setup(self):
+    def setUp(self):
         self.response = home(HttpRequest())
 
     def test_root_url_resolves_to_home_page(self):
@@ -15,26 +15,24 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home)
 
     def test_home_page_return_html_response(self):
-        response = home(HttpRequest())
-        self.assertIsInstance(response, HttpResponse)
+        self.assertIsInstance(self.response, HttpResponse)
 
     def test_home_view_renders_a_template(self):
-        response = home(HttpRequest())
         expected_html = render_to_string('home.html')
-        self.assertEqual(response.content, expected_html)
+        self.assertEqual(self.response.content, expected_html)
 
     def test_home_page_has_right_title(self):
-        response = home(HttpRequest())
-        self.assertIn('<title>Mini Shine</title>', response.content)
+        self.assertIn('<title>Mini Shine</title>', self.response.content)
 
     def test_home_page_has_right_header(self):
-        response = home(HttpRequest())
-        self.assertIn('<h1>Mini Shine</h1>', response.content)
+        self.assertIn('<h1>Mini Shine</h1>', self.response.content)
 
     def test_home_page_has_register_now_link(self):
-        response = home(HttpRequest())
-        self.assertRegexpMatches(str(response.content), r'<a.*>Register Now!</a>')
+        self.assertRegexpMatches(str(self.response.content), r'<a.*>Register Now!</a>')
 
     def test_register_now_link_has_right_url(self):
-        response = home(HttpRequest)
+        self.assertRegexpMatches(str(self.response.content), r'<a.*?/register/.*?>Register Now!</a>', 'Register Now link doesn\'t have right url')
+
+
+
 
